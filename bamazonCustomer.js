1ff31c86f37a1d2667ad5cda0e0c2d080f9589a8
AustinSFleet <austinsfleet@gmail.com>
2017-09-13 13:53:25 -0500

    fixed it

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,6 +20,41 @@ connection.query(
         for (i=0; i<res.length; i++){
         console.log("=====================================\n");
         console.log("Product ID: " + res[i].product_id + " || Product Name: " + res[i].product_name + " || PRICE : $" + res[i].price);
-        }
+        
+    }
+    buySomething();
     }
 );
+
+
+
+function buySomething (){
+    inquirer
+    .prompt([
+        {
+        type: "input",
+        name: "product",
+        message: "Input the product number of the item you'd like to buy"
+        },
+        {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to buy?",
+        validate: function(value){
+            if (isNaN(value) === false){
+                return true;
+            }
+            return false;
+        }
+
+        },
+    ]).then(function(answer){
+        var query = "SELECT product_id, stock_quantity FROM products WHERE id = ?";
+            connection.query(query, answer.product, function(err, res){
+                    console.log("You bought " + answer.quantity + " " + "product_name" + "s")
+            })
+    })
+};
+
+
+
